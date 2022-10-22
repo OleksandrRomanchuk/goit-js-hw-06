26b4d0842ababc21943fc9ef352275ba5430bd34
Oleksandr Romanchuk <novator0511@gmail.com>
2022-10-22 15:57:15 +0300

    task#8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,30 @@
+const FORM_SELECTOR = ".login-form";
+const loginForm = document.querySelector(FORM_SELECTOR);
 
+loginForm.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (!formFieldsValidation(loginForm)) return;
+
+  const formData = [...event.target].reduce((dataObj, { name }) => {
+    if (name) {
+      dataObj[name] = event.target.elements[name].value;
+    }
+
+    return dataObj;
+  }, {});
+
+  console.log(formData);
+
+  event.target.reset();
+}
+
+function formFieldsValidation(form) {
+  const isFullField = document.querySelectorAll(`${FORM_SELECTOR} input`);
+
+  return [...isFullField].some((field) => field.value)
+    ? true
+    : alert("Hello! All fields must be filled!");
+}
