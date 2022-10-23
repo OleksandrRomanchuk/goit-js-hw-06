9191efc192cd9c4de5714acef79f2b45a7e6175c
Oleksandr Romanchuk <novator0511@gmail.com>
2022-10-23 10:19:02 +0300

    task#9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const refs = {
+  bodyEl: document.querySelector("body"),
+  buttonEl: document.querySelector(".change-color"),
+  outputEl: document.querySelector(".color"),
+};
+
+refs.buttonEl.addEventListener("click", onClkChangeColor);
+
+function onClkChangeColor() {
+  const newColor = getRandomHexColor();
+  refs.bodyEl.style.backgroundColor = newColor;
+  refs.outputEl.textContent = newColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
