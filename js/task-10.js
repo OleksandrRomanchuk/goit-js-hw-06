70c7ea60de3d552440d675c7835f9594bf283dd3
Oleksandr Romanchuk <novator0511@gmail.com>
2022-10-23 18:44:49 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,48 +1,49 @@
 const refs = {
-    inputEL: document.querySelector('input'),
-    createBtn: document.querySelector('button[data-create]'),
-    destroyBtn: document.querySelector('button[data-destroy]'),
-    outputEl: document.querySelector('#boxes'),
+  inputEL: document.querySelector("input"),
+  createBtn: document.querySelector("button[data-create]"),
+  destroyBtn: document.querySelector("button[data-destroy]"),
+  outputEl: document.querySelector("#boxes"),
 };
 
-refs.createBtn.addEventListener('click', renderBoxes);
+refs.createBtn.addEventListener("click", renderBoxes);
 
-refs.destroyBtn.addEventListener('click', destroyBoxes);
+refs.destroyBtn.addEventListener("click", destroyBoxes);
 
 function renderBoxes() {
-    const amount = getInputValue();
+  refs.outputEl.innerHTML = "";
 
-    refs.outputEl.append(...createBoxes(amount));
+  const amount = getInputValue();
+
+  refs.outputEl.append(...createBoxes(amount));
 }
 
 function getInputValue() {
-    const amount = !Number(refs.inputEL.value)
-        ? alert('Oops! Please enter the number of boxes you want to create.')
-        : refs.inputEL.value;
+  const amount = !Number(refs.inputEL.value)
+    ? alert("Oops! Please enter the number of boxes you want to create.")
+    : refs.inputEL.value;
 
-    return amount;
+  return amount;
 }
 
 function createBoxes(amount) {
-    destroyBoxes();
+  const boxes = [];
 
-    const boxes = [];
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = 30 + i * 10 + "px";
+    box.style.height = box.style.width;
+    boxes.push(box);
+  }
 
-    for (let i = 0; i < amount; i += 1) {
-        const box = document.createElement('div');
-        box.style.backgroundColor = getRandomHexColor();
-        box.style.width = 30 + i * 10 + 'px';
-        box.style.height = box.style.width;
-        boxes.push(box);
-    }
-
-    return boxes;
+  return boxes;
 }
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 function destroyBoxes() {
-    refs.outputEl.innerHTML = '';
+  refs.outputEl.innerHTML = "";
+  refs.inputEL.value = "";
 }
